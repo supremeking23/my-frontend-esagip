62348cfd4a73209d30e4753485cc6d0571705b12
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2020-12-16 01:31:59 +0800

    second action

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 					href="https://reactjs.org"
 					target="_blank"
 					rel="noopener noreferrer">
-					Learn React ssss
+					Learn React sssssddssds
 				</a>
 			</header>
 		</div>
