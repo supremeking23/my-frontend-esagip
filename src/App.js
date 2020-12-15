afeec363dd1a6df8d01259502fa3c0c4ab6afd9b
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2020-12-16 01:59:53 +0800

    added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import { Switch, Route } from "react-router-dom";
+
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
-				<h1>Dito nalang</h1>
-			</header>
+			<Switch>
+				<Route exact={true} path="/" component={Homepage} />
+				<Route exact={true} path="/dashboard" component={Dashboard} />
+			</Switch>
 		</div>
 	);
 }
+
+const Homepage = () => {
+	return (
+		<>
+			<h1>Homepage</h1>
+		</>
+	);
+};
+
+const Dashboard = () => {
+	return (
+		<>
+			<h1>Dashboard</h1>
+		</>
+	);
+};
 
 export default App;
